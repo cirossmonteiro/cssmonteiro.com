25a679081626f4bd7cf76ea2dc3e42fa45701e80
Ciro Monteiro <cirossmonteiro@gmail.com>
2023-01-12 12:40:06 -0300

    file-manager rename

--- src/components/file-manager/index.tsx
+++ src/components/file-manager/index.tsx
@@ -1,8 +1,8 @@
-import { Table as AntDTable, Button, Checkbox, Form, Input, Tooltip } from "antd";
-import { faCircleCheck, faPencil, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Table as AntDTable, Button, Checkbox, Form, Input, Modal, Tooltip } from "antd";
+import { faCircleCheck, faDownload, faPencil, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import styled from "styled-components";
 
@@ -51,11 +51,17 @@ const allowUncompress = (file: any) => {
   return file.filename.slice(-4) === ".zip";
 }
 
+const rename = async (oldName: string, newName: string) => {
+  const response = await axiosInstance.patch("/file-manager", { oldName, newName });
+  return response.data;
+}
+
 
 const FileManager = () => {
   const [currentFolder, setCurrentFolder] = useState<string>("");
   const [uploaded, setUploaded] = useState<boolean[]>([]);
   const [listing, setListing] = useState<any[]>([]);
+  const [openRenameModal, setOpenRenameModal] = useState<number>(-1);
 
   const { control, handleSubmit, setValue } = useForm({
     defaultValues: {
@@ -66,6 +72,19 @@ const FileManager = () => {
     control,
     name: "file",
   });
+
+  const { handleSubmit: handleRenameSubmit, control: renameControl, setValue: setRenameValue } = useForm({
+    defaultValues: {
+      filename: "",
+    }
+  });
+
+  useEffect(() => {
+    if (openRenameModal !== -1) {
+      setRenameValue("filename", listing[openRenameModal].filename);
+    }
+  }, [openRenameModal]);
+  
 
   const uploadFiles = useCallback(async (e: any) => {
     const data = await readFileList(e.target.files);
@@ -123,11 +142,6 @@ const FileManager = () => {
     });
   }, [currentFolder]);
 
-  // to-do: rename after upload
-  const handleRename = useCallback((filename: string) => () => {
-
-  }, []);
-
   const onSelectChange = useCallback((filenames: React.Key[]) => {
     setListing(list => {
       return list.map(file => {
@@ -163,6 +177,12 @@ const FileManager = () => {
   const handleUploadRemove = useCallback((index: number) => () => {
     removeFromUploadList(index);
   }, []);
+
+  const handleRename = useCallback(async (values: any) => {
+    const { filename } = listing[openRenameModal];
+    await rename(`${currentFolder}/${filename}`, `${currentFolder}/${values.filename}`);
+    setOpenRenameModal(-1);
+  }, [openRenameModal]);
 
   return (
     <div>
@@ -269,23 +289,51 @@ const FileManager = () => {
           },
           ...(isListingEdited ? [] : [{
             title: "actions",
-            render: (_: any, file: any) => {
+            render: (_: any, file: any, index: number) => {
               return  file.filename === ".." ? null
               : <>
                   <Tooltip title="delete">
                     <FAIconClickable icon={faTrashCan} onClick={handleDelete(file.filename)} />
                   </Tooltip>
                   <Tooltip title="rename">
-                    <FAIconClickable className="ms-2" onClick={handleRename(file.filename)} icon={faPencil} />
+                    <FAIconClickable className="ms-2" onClick={() => setOpenRenameModal(index)} icon={faPencil} />
                   </Tooltip>
+                  {!file.isDirectory && (
+                    <Tooltip title="access">
+                      <A href={`http://localhost:4000/media${currentFolder}/${file.filename}`} className="ms-2" target="_blank">
+                        <FontAwesomeIcon icon={faDownload} />
+                      </A>
+                    </Tooltip>
+                  )}
                 </>;
             }
           }])
         ]}
       />
+
+      <Modal open={openRenameModal !== -1} onCancel={() => setOpenRenameModal(-1)}
+        onOk={handleRenameSubmit(handleRename)}>
+        <Form>
+          <Form.Item label="Current name">
+            <Input value={openRenameModal === -1 ? "" : listing[openRenameModal].filename} />
+          </Form.Item>
+          <Form.Item label="New name">
+            <Controller
+              name="filename"
+              control={renameControl}
+              rules={{ required: true }}
+              render={({ field }) => <Input {...field} />}
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   )
 }
+
+const A = styled.a`
+  color: gray;
+`
 
 const Upload = styled.label`
   input {
